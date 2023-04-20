9c9f34c0f179b057f434b39f4e3e63c5d203e9d5
Guilherme Jorge <guilherme.jorge010@gmail.com>
2023-04-20 12:23:57 -0300

    fix: deletei a checagem de parâmetro da função salvarDadosPessoais

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,58 +19,6 @@ export const salvarDadosPessoais = functions
 
   // Seleçõa do tipo de chamda da função
   .https.onCall(async (data) => {
-    // ? Checagem pode ser redundante, podendo ser feita diretamente no app
-    // * COMEÇO DA CHECAGEM DE PARÂMETROS
-    // Checagem para ver se o parâmetro nome está vazio ou não
-    if (!(typeof data.nome === "string") || data.nome.length === 0) {
-      throw new functions.https.HttpsError(
-        "invalid-argument",
-        "A função deve ser chamada com " +
-          "um argumento 'nome' contendo o nome do profissional a ser adcionado"
-      );
-    }
-
-    // Checagem para ver se o parâmetro nome está vazio ou não
-    if (!(typeof data.telefone === "string") || data.telefone.length === 0) {
-      throw new functions.https.HttpsError(
-        "invalid-argument",
-        "A função deve ser chamada com " +
-          "um argumento 'telefone' contendo " +
-          "o telefone do profissional a ser adcionado"
-      );
-    }
-
-    // Checagem para ver se o parâmetro nome está vazio ou não
-    if (!(typeof data.email === "string") || data.email.length === 0) {
-      throw new functions.https.HttpsError(
-        "invalid-argument",
-        "A função deve ser chamada com " +
-          "um argumento 'email' contendo " +
-          "o email do profissional a ser adcionado"
-      );
-    }
-
-    // Checagem para ver se o parâmetro nome está vazio ou não
-    if (!(typeof data.endereco1 === "string") || data.endereco1.length === 0) {
-      throw new functions.https.HttpsError(
-        "invalid-argument",
-        "A função deve ser chamada com " +
-          "um argumento 'endereco1' contendo " +
-          "o endereço principal do profissional a ser adcionado"
-      );
-    }
-
-    // Checagem para ver se o parâmetro nome está vazio ou não
-    if (!(typeof data.curriculo === "string") || data.curriculo.length === 0) {
-      throw new functions.https.HttpsError(
-        "invalid-argument",
-        "A função deve ser chamada com " +
-          "um argumento 'curriculo' contendo " +
-          "o currículo principal do profissional a ser adcionado"
-      );
-    }
-    // * FIM DA CHECAGEM DE PARÂMETROS
-
     // Tentativa de adição de dados no banco de dados Firebase Firestore
     try {
       await colProfissionais.doc().set(data);
