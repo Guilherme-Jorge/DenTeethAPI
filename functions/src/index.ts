b458f26b2b049d83ecfb4b95610e07488035a7e1
Guilherme Jorge <guilherme.jorge010@gmail.com>
2023-05-05 14:06:48 -0300

    feat: criei a funcao de notificacao de emergencia e de resposta do chamado de emergencia

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,6 +10,8 @@ const db = app.firestore();
 
 // Salva acesso a coleção "profissionais" no Firestore Database
 const colProfissionais = db.collection("profissionais");
+// const colEmergencia = db.collection("emergencia");
+const colRespostas = db.collection("respostas");
 
 type Profissional = {
   nome: string;
@@ -92,18 +94,18 @@ export const salvarDadosPessoais = functions
     return JSON.stringify(cResponse);
   });
 
-export const NotifyNewEmergence = functions
-  .region("southamerica-east1").firestore
-  .document("emergencias/{userId}")
+export const NotifyNewEmergency = functions
+  .region("southamerica-east1")
+  .firestore.document("emergencias/{userId}")
   .onCreate(async (snap) => {
-    let status = false;
     const newValue = snap.data();
 
     // Log para ver os dados da Emergencia
     console.log(newValue);
 
-    const onProfissionais = (await colProfissionais
-      .where("status", "==", true).get());
+    const onProfissionais = await colProfissionais
+      .where("status", "==", true)
+      .get();
 
     // Pegar os dados de todos os Profissionais onlines
     onProfissionais.docs.forEach(async (doc) => {
@@ -118,9 +120,10 @@ export const NotifyNewEmergence = functions
               nome: newValue.nome,
               telefone: newValue.telefone,
               fotos: newValue.fotos,
-              status: newValue.status,
+              status: "NOVA",
               descricao: newValue.descricao,
               dataHora: newValue.dataHora,
+              id: snap.id,
             },
             token: fcmToken,
           };
@@ -130,12 +133,76 @@ export const NotifyNewEmergence = functions
 
           // Log para ver o Id da mensagem enviada
           console.log(messageId);
-          status = true;
         } catch (e) {
           log(e);
         }
       }
     });
+  });
 
-    return JSON.stringify({ status: status });
+// TODO: Utilizar multicast na funcao acima
+// exports.OnCreateEmergence = functions.firestore
+//   .document("energencias/{userId}")
+//   .onCreate(async (snap, context) => {
+//     const newValue = snap.data();
+
+//     const res = await colProfissionais.where("status", "==", true).get();
+//     const tokens = res.docs.map((doc) => doc.data().fcmToken);
+//     console.log(tokens);
+//     const message = {
+//       data: {
+//         text: "Nova Emergencia",
+//       },
+//       tokens: tokens,
+//       newValue,
+//     };
+//     try {
+//       await app.messaging().sendMulticast(message);
+//     } catch (e) {
+//       console.log("Erro", e);
+//     }
+//   });
+
+export const responderChamado = functions
+
+  // Seleção da região que a função irá ficar
+  .region("southamerica-east1")
+
+  // Habilitar a checagem de aplicativo ou não
+  .runWith({ enforceAppCheck: false })
+
+  // Seleçõa do tipo de chamda da função
+  .https.onCall(async (data) => {
+    const cResponse: CustomResponse = {
+      status: "ERROR",
+      message: "Dados não fornecidos",
+      payload: undefined,
+    };
+
+    // Tentativa de adição de dados no banco de dados Firebase Firestore
+    try {
+      const doc = await colRespostas.add(data);
+      if (doc.id != undefined) {
+        cResponse.status = "SUCCESS";
+        cResponse.message = "Resposta inserida";
+        cResponse.payload = JSON.stringify({ docId: doc.id });
+      } else {
+        cResponse.status = "ERROR";
+        cResponse.message = "Não foi possível inserir a resposta da emergencia";
+        cResponse.payload = JSON.stringify({ errorDetail: "doc.id" });
+      }
+    } catch (e) {
+      let exMessage;
+      if (e instanceof Error) {
+        exMessage = e.message;
+      }
+      functions.logger.error("Erro ao incluir resposta:", data.email);
+      functions.logger.error("Exception: ", exMessage);
+      cResponse.status = "ERROR";
+      cResponse.message = "Erro ao incluir resposta - Verificar Logs";
+      cResponse.payload = null;
+    }
+
+    // Mensagem com informações sobre o resultado da função
+    return JSON.stringify(cResponse);
   });
